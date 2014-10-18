2aad4d88be1f48db8985b7b1071a5c529757f34f
Anders Ro <anders.ronnbrant@gmail.com>
2014-10-18 17:26:15 +0200

    Add code test for enum class as compile time index in std::array
    Seems to work. User declares enum class and provides that as template
    argument to the recorder. Recording can than be done using the provided
    enum class as argument. Using the wrong enum class causes compiler
    error, very good for preventing mistakes. Only problem is finding out
    the number of values in the enum, and make sure the user do not set the
    values explicitly in the enum class.
    
    By creating a template helper function that at compile time checks for
    out-of-range indexing in the array it should not cause any problems with
    unique enum values, as long the as the maximum element is named "Count"
    or similar. Not providing that element in the enum class is also a
    compile time error.

--- makerules.mk
+++ makerules.mk
@@ -13,7 +13,7 @@ $(call setup)
 _CXXFLAGS := -std=c++11
 _LDFLAGS  := -rdynamic -Wl,-rpath=../lib -Wl,-rpath=./lib -Wl,-rpath=$(TGTDIR)
 
-TARGETS := recordertest servertest
+TARGETS := recordertest servertest foo
 
 recordertest_SRCS := src/main_recorder.cpp src/Recorder.cpp src/proto/*.cc
 recordertest_USES := zeromq protobuf
@@ -22,5 +22,7 @@ recordertest_LINK := zmq protobuf pthread
 servertest_SRCS := src/main_server.cpp
 servertest_USES := zeromq
 servertest_LINK := zmq pthread
+
+foo_SRCS := src/main_foo.cpp
 
 include $(FOOTER)
